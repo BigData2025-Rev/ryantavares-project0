83cc9978eacdd94ad11db6955c7c65991dc0e501
ortava <RyanTommyTavares@gmail.com>
2024-12-24 08:53:59 -0800

    Fix damage attribute assignment and add __str__ method

--- parcel.py
+++ parcel.py
@@ -4,4 +4,7 @@ class Parcel:
     def __init__(self, name, weight, damage=0.00):
         self.name = name
         self.weight = weight
-        
+        self.damage = damage
+    
+    def __str__(self):
+        return f'Parcel(name={self.name}, weight={self.weight}, damage={self.damage})'
