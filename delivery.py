7af9780b001f44c6bba3ab8049b9d5d8dde9bf59
ortava <RyanTommyTavares@gmail.com>
2024-12-24 13:29:04 -0800

    Add generate_parcels method

--- delivery.py
+++ delivery.py
@@ -1,5 +1,7 @@
 """This file defines a delivery object."""
 
+from parcel import Parcel
+
 class Delivery:
     def __init__(self, key, title, name_of_parcels, num_of_parcels, total_weight, destination):
         self.key = key
@@ -8,4 +10,10 @@ class Delivery:
         self.num_of_parcels = num_of_parcels
         self.total_weight = total_weight
         self.destination = destination
-        
+    
+    def generate_parcels(self) -> list[Parcel]:
+        parcels = []
+        weight_per = self.total_weight / self.num_of_parcels
+        for i in range(self.num_of_parcels):
+            parcels.append(Parcel(self.name_of_parcels, weight_per))
+        return parcels
