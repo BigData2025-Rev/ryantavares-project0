687dc20cb13aa597bdbb365d7915a531187d7372
ortava <RyanTommyTavares@gmail.com>
2024-12-25 14:14:04 -0800

    Add 'key' and 'coords' attributes to depo objects

--- depo.py
+++ depo.py
@@ -3,6 +3,11 @@
 from delivery import Delivery
 
 class Depo:
-    def __init__(self, name, deliveries):
+    def __init__(self, key, name, coords, deliveries):
+        self.key = key
         self.name = name
+        self.coords = coords
         self.deliveries = [Delivery(**delivery) for delivery in deliveries]
+
+    def distance_to(self, destination_coords):
+        return (destination_coords['x']**2 + destination_coords['y']**2)**0.5
