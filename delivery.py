c391d682d87ea11dff9ecee19b6180857997b6b8
ortava <RyanTommyTavares@gmail.com>
2024-12-27 14:05:25 -0800

    Update format for recording completion of deliveries

--- delivery.py
+++ delivery.py
@@ -24,11 +24,15 @@ class Delivery:
     def record(self, num_delivered: int, total_damage: float, time_completed: dt.datetime = None):
         # Write results of the delivery to a csv file.
         file_name = f'records/delivery-results.csv'
+        completion_rate = num_delivered / self.num_of_parcels
+        damage_rate = total_damage / self.num_of_parcels
+        minutes_to_complete = round((time_completed - self.time_activated).seconds / 60, 2)
         data = {
             'delivery-key': self.key,
-            'completion-rate': num_delivered / self.num_of_parcels,
-            'damage-rate': total_damage / self.num_of_parcels,
-            'time-to-complete': (time_completed - self.time_activated).seconds}
+            'completion-rate': completion_rate,
+            'damage-rate': damage_rate,
+            'minutes-to-complete': minutes_to_complete
+            }
         with open(file_name, 'a') as outfile:
             writer = csv.DictWriter(outfile, fieldnames=data)
             if outfile.tell() == 0:
@@ -36,6 +40,10 @@ class Delivery:
             writer.writerow(data)
         
         # Give some immediate information to the user.
-        print(f"{num_delivered} {self.name_of_parcels}")
-        print(f"Completion Rate: {num_delivered / self.num_of_parcels:.2f}")
-        print(f"Damage Rate: {total_damage / self.num_of_parcels:.2f}\n")
+        width = 30
+        print(f"D{self.key} RESULT".center(width, '='))
+        print(f"{num_delivered} / {self.num_of_parcels} {self.name_of_parcels}")
+        print(f"Completion Rate: {completion_rate:.2f}")
+        print(f"Damage Rate: {damage_rate:.2f}")
+        print(f"Time to Complete: {minutes_to_complete:.2f} minutes")
+        print("".center(width, '='))
